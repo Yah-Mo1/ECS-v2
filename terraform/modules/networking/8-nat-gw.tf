e1499be847f28fdcef3526bdca4c2f17491babde
Yah-Mo1 <yahyamohamed1231@outlook.com>
2025-10-30 22:46:56 +0000

    Modify Terraform Apply and Destroy Workflows

--- terraform/modules/networking/8-nat-gw.tf
+++ terraform/modules/networking/8-nat-gw.tf
@@ -1,17 +1,17 @@
-resource "aws_eip" "nat_eip" {
-  #   vpc        = true
-  count      = length(data.aws_availability_zones.available.names)
-  depends_on = [aws_internet_gateway.this]
-}
+# resource "aws_eip" "nat_eip" {
+#   #   vpc        = true
+#   count      = length(data.aws_availability_zones.available.names)
+#   depends_on = [aws_internet_gateway.this]
+# }
 
-# NAT
-resource "aws_nat_gateway" "this" {
-  count         = length(data.aws_availability_zones.available.names)
-  allocation_id = element(aws_eip.nat_eip.*.id, count.index)
-  subnet_id     = element(aws_subnet.public_subnet.*.id, count.index)
-  tags = {
-    Name        = "nat"
-    Environment = var.env
-  }
-  depends_on = [aws_internet_gateway.this, aws_eip.nat_eip]
-}
+# # NAT
+# resource "aws_nat_gateway" "this" {
+#   count         = length(data.aws_availability_zones.available.names)
+#   allocation_id = element(aws_eip.nat_eip.*.id, count.index)
+#   subnet_id     = element(aws_subnet.public_subnet.*.id, count.index)
+#   tags = {
+#     Name        = "nat"
+#     Environment = var.env
+#   }
+#   depends_on = [aws_internet_gateway.this, aws_eip.nat_eip]
+# }
